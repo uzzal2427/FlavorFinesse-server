6ead0b51fee53fe4f8cc3589c7f7272f37edf9f4
uzzal2427 <uzzal2427ahmed@gmail.com>
2023-09-29 19:02:42 +0300

    add more data

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 const chef = require('./Data/chef.json')
+var cors = require('cors')
 const port = 3000
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -9,6 +12,12 @@ app.get('/', (req, res) => {
 
 app.get('/chef', (req,res)=>{
     res.send(chef);
+})
+
+app.get('/chef/:id', (req,res)=>{
+  const id = req.params.id;
+  const singleChef = chef.find((c)=> c.unique_id == id);
+  res.send(singleChef);
 })
 
 app.listen(port, () => {
